dcb7acc7dcda1346079a4870aa26fd73ec0ff5ac
Ubuntu <ubuntu@ip-10-0-1-180.ec2.internal>
2023-11-14 23:20:16 +0000

    training file init

---
+++ Code/train_classifier.py
@@ -0,0 +1,7 @@
+#### Imports
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix, classification_report
+import torch
